81719f12e4a9fc710f1461988880a2d5eaaa6150
Daniel Rudrich <mail@danielrudrich.de>
2018-12-21 13:18:05 +0100

    adds license information to source files

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,12 +1,24 @@
 /*
-  ==============================================================================
+ ==============================================================================
 
-    This file was auto-generated!
+ ABComparison Plug-in
+ Copyright (C) 2018 - Daniel Rudrich
 
-    It contains the basic framework code for a JUCE plugin processor.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-  ==============================================================================
-*/
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ ==============================================================================
+ */
 
 /*
  ===== TODOs =====
